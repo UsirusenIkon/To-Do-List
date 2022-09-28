6514719045cdd1c00eb271492ac2d49e5c188512
Usirusen Ikon <lexicon048@gmail.com>
2022-09-28 12:09:55 +0100

    create modules

---
+++ src/modules/TodoListArray.js
@@ -0,0 +1,36 @@
+class TodoListArray {
+  constructor() {
+    this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  updateTodo(idx, newTodo) {
+    this.todos[idx - 1] = newTodo;
+    for (let i = 0; i < this.todos.length; i += 1) {
+      this.todos[i].idx = i + 1;
+    }
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  deleteTodo(index) {
+    if (this.todos.length === 1) {
+      this.todos = [];
+    } else {
+      this.todos.splice(index, 1);
+    }
+    for (let i = 0; i < this.todos.length; i += 1) {
+      this.todos[i].idx = i + 1;
+    }
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  getAllTodos() {
+    return this.todos;
+  }
+}
+
+export default TodoListArray;
---
+++ src/modules/TodoListItem.js
@@ -0,0 +1,9 @@
+class TodoListItem {
+  constructor(description, completed, idx) {
+    this.description = description;
+    this.completed = completed;
+    this.index = idx;
+  }
+}
+
+export default TodoListItem;
